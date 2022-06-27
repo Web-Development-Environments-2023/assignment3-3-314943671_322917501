7e8d60808c63a739d0832ed627c15314d15eb70a
ChenKolely <kolelyh@pop-os.localdomain>
2022-06-27 16:28:27 +0300

    fixed server_connection

--- server_connection.js
+++ server_connection.js
@@ -1,37 +1,28 @@
 var path = require("path");
-var app = require('./app');
-// var debug = require('debug')('modamedic:server');
-// var http = require('http');
+var app = require('./main');
 var https = require('https');
 var fs = require('fs');
 
 var httpsOptions = {
-  key: fs.readFileSync(path.join(__dirname,"server.key")),
-  // key: fs.readFileSync('server.key'),
-  // key: fs.readFileSync("C:\\Users\\noyha\\IdeaProjects\\ProjectServer\\key.pem"),
-  // cert: fs.readFileSync("C:\\Users\\noyha\\IdeaProjects\\ProjectServer\\cert.pem")  key: fs.readFileSync("C:\\Users\\noyha\\IdeaProjects\\ProjectServer\\key.pem"),
-  cert: fs.readFileSync(path.join(__dirname,"server.cert")),
-
+  key: fs.readFileSync(path.join(__dirname, "privkey.pem")),//server.key
+  cert: fs.readFileSync(path.join(__dirname, "fullchain.pem")),//server.cert
 }
+
 /**
  * Get port from environment and store in Express.
  */
-
-// var port = normalizePort(process.env.PORT || '8181');
 var port = normalizePort(process.env.PORT || '443');
-// var port = process.env.PORT;
+
 app.set('port', port);
 
 /**
  * Create HTTP server.
  */
-
 var server = https.createServer(httpsOptions, app);
 
 /**
  * Listen on provided port, on all network interfaces.
  */
-
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
@@ -39,7 +30,6 @@ server.on('listening', onListening);
 /**
  * Normalize a port into a number, string, or false.
  */
-
 function normalizePort(val) {
   var port = parseInt(val, 10);
 
@@ -59,7 +49,6 @@ function normalizePort(val) {
 /**
  * Event listener for HTTP server "error" event.
  */
-
 function onError(error) {
   if (error.syscall !== 'listen') {
     throw error;
@@ -67,7 +56,6 @@ function onError(error) {
   var bind = typeof port === 'string'
     ? 'Pipe ' + port
     : 'Port ' + port;
-
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
@@ -86,13 +74,11 @@ function onError(error) {
 /**
  * Event listener for HTTP server "listening" event.
  */
-server.address("http://recipeideas.cs.bgu.ac.il");
+server.address("https://hallel-pro.cs.bgu.ac.il");
 function onListening() {
-
   var addr = server.address();
   var bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
     console.log(`Server listen in port ${port} in adrress ${addr.address}`);
-//   debug('Listening on ' + bind);
 }
